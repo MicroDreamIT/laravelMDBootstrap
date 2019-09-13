2597815952b4dde424fef7be0fc55ac129c39b3e
MicroDreamIT <shahanur700@gmail.com>
2019-09-13 21:34:00 +0600

    first

---
+++ resources/js/components/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        I am dashboard
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "dashboard"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from '@/components/Dashboard'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      props: { page: 1 },
+      alias: '/'
+    },
+  ]
+})
